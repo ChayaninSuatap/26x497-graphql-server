f11b60c483df96760a1eedb7080e9667a44bafcc
Chayanin.s <chayanin.s@cmu.ac.th>
2023-08-08 08:59:41 +0700

    done

---
+++ src/typeDefs.ts
@@ -0,0 +1,37 @@
+export const typeDefs = `#graphql
+  type User {
+    username: String!
+    password: String!
+    firstName: String!
+    lastName: String!
+    posts: [Post]!
+  }
+
+  type Post {
+    postId: String!
+    postText: String!
+    dateISOString: String!
+    comments: [Comment]
+    username: String!
+    user: User!
+  }
+
+  type Comment {
+    commentId: String!
+    commentText: String!
+    dateISOString: String!
+    postId: String!
+    username: String!
+    user: User!
+  }
+
+  type Query {
+    posts: [Post]
+    user(username: String!): User
+  }
+
+  type Mutation {
+    addPost(postText: String!, username: String!): Post
+    addComment(postId: String!, commentText: String!, username: String!): Comment
+  }
+`;
